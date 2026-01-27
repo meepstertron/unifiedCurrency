e97baeedc30f955d6aff855e037272c92d5e76d3
meepstertron <meepstertron@gmail.com>
2026-01-27 19:55:09 +0100

    Initial commit

---
+++ src/main/java/org/hexagonical/unifiedcurrency/Unifiedcurrency.java
@@ -0,0 +1,10 @@
+package org.hexagonical.unifiedcurrency;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Unifiedcurrency implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
